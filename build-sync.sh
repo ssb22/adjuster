a7b0f37232d84b9854356a20836a790a4305a248
Silas S. Brown <ssb22@cam.ac.uk>
2015-04-17 05:42:03 +0000

    test commit
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/adjuster@2161 29193198-4895-4776-b068-10539e920549

--- build-sync.sh
+++ build-sync.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # sync Web Adjuster and Annotator Generator to SVN
+# also TermLayout
 wget -N http://people.ds.cam.ac.uk/ssb22/adjuster/adjuster.py
 wget -N http://people.ds.cam.ac.uk/ssb22/adjuster/annogen.py
 wget -N http://people.ds.cam.ac.uk/ssb22/adjuster/termlayout.py
