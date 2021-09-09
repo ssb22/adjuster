f1a405cd957e1c3fd52d0d34d661b809a3cad44e
Silas S. Brown <ssb22@cam.ac.uk>
2021-09-09 12:53:28 +0100

    freebsd port lint etc

---
+++ freebsd/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ssh freebsd pkg install portlint
+# ssh freebsd "echo 'DEVELOPER=yes' >> /etc/make.conf"
+scp Makefile pkg-descr freebsd:/usr/ports/www/adjuster/ || exit 1
+ssh freebsd 'cd /usr/ports/www/adjuster/ && rm -f distinfo && make makesum && portlint -A && git add * && git commit * -m "www/adjuster '"$(grep -m 1 '^program_name' ../adjuster.py|cut -d ' ' -f5)"'" && git format-patch --stdout -1' > adjuster.mbox &&
+echo "adjuster.mbox to https://bugs.freebsd.org/bugzilla/enter_bug.cgi (as plain-text attachment if possible)"
