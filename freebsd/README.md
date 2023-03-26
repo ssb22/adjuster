50733686f59651d1224a84e5a7b830f70866b9b0
Silas S. Brown <ssb22@cam.ac.uk>
2023-03-26 13:55:38 +0100

    FreeBSD update

--- freebsd/README.md
+++ freebsd/README.md
@@ -1,2 +1,3 @@
 
 * Submitted v3.149 as [issue 258459](https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=258459)
+  * later updated to v3.23
--- freebsd/update.sh
+++ freebsd/update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 # ssh freebsd pkg install portlint
 # ssh freebsd "echo 'DEVELOPER=yes' >> /etc/make.conf"
-scp Makefile pkg-descr freebsd:/usr/ports/www/adjuster/ || exit 1
-ssh freebsd 'cd /usr/ports/www/adjuster/ && rm -rf work distinfo && make makesum && portlint -A && git add * && git commit * -m "www/adjuster '"$(grep -m 1 '^"Web' ../adjuster.py|cut -d ' ' -f3)"'"' &&
-ssh freebsd 'cd /usr/ports && git format-patch --stdout -1' > adjuster.mbox &&
+ssh freebsd mkdir -p /usr/ports/www/adjuster/
+scp Makefile pkg-descr freebsd:/usr/ports/www/adjuster/
+ssh freebsd 'cd /usr/ports/www/adjuster/ && touch distinfo && git add Makefile pkg-descr distinfo'
+ssh freebsd 'cd /usr/ports/www/adjuster/ && rm -rf work distinfo && make makesum && portlint -A && git add * && git commit * -m "www/adjuster '"$(grep -m 1 '^"Web' ../adjuster.py|cut -d ' ' -f3)"'"'
+ssh freebsd 'cd /usr/ports && git format-patch --stdout -1' > adjuster.mbox
 echo "adjuster.mbox to https://bugs.freebsd.org/bugzilla/enter_bug.cgi (as plain-text attachment if possible)"
